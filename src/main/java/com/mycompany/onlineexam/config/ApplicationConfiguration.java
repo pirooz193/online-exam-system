1b6645be019f48afaa3c7d6ab74c757e8caca447
Pirooz Azar Abad <piroozazarabad@gmail.com.com>
2023-05-08 21:22:05 +0330

    Add swagger to generate document from Apis. - Crate configuration class to config Docket for apis. - Set the public access to get token. - add opeApi and spring validation dependencies ro the pom.xml

--- src/main/java/com/mycompany/onlineexam/config/ApplicationConfiguration.java
+++ src/main/java/com/mycompany/onlineexam/config/ApplicationConfiguration.java
@@ -1,21 +1,47 @@
-//package com.mycompany.onlineexam.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-///**
-// * User: Pirooz
-// * Date : 5/3/2022
-// * Time : 10:56 AM
-// */
-//@Configuration
-//@EnableWebMvc
-//public class ApplicationConfiguration implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("http://localhost:5500/**") ;
-//    }
-//}
+package com.mycompany.onlineexam.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@Configuration
+@EnableWebMvc
+public class ApplicationConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .securitySchemes(Collections.singletonList(apiKey()))
+                .securityContexts(Collections.singletonList(securityContext()))
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.mycompany"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Bearer", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(Collections.singletonList(securityReference()))
+                .forPaths(PathSelectors.regex("(?!/public/).*")) // exclude paths containing "/public/"
+                .build();
+    }
+
+    private SecurityReference securityReference() {
+        return new SecurityReference("Bearer", new AuthorizationScope[0]);
+    }
+}
